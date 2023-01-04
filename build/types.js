8cf3938d23deca8b92340823fb4e015e442f2c35
alinekabbas <alinekabbas@gmail.com>
2023-01-04 16:37:46 -0300

    labecommerce backend

--- build/types.js
+++ build/types.js
@@ -1,3 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CATEGORIES = void 0;
+var CATEGORIES;
+(function (CATEGORIES) {
+    CATEGORIES["ELETROPORTATEIS"] = "eletroport\u00E1teis";
+    CATEGORIES["ELETRONICOS"] = "eletr\u00F4nicos";
+    CATEGORIES["MOVEIS"] = "m\u00F3veis";
+})(CATEGORIES = exports.CATEGORIES || (exports.CATEGORIES = {}));
 //# sourceMappingURL=types.js.map
