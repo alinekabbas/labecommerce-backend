b372526d4b7eac0d1ba2511de75095cb59d77664
alinekabbas <alinekabbas@gmail.com>
2023-01-10 17:35:54 -0300

    labecommerce backend

--- src/index.ts
+++ src/index.ts
@@ -74,3 +74,95 @@ app.post("/purchases", (req:Request, res: Response)=>{
     purchases.push(newPurchase)
     res.status(201).send("Compra realizada com sucesso")
 })
+
+app.get("/products/:id", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const result = products.filter((product)=>{
+        return product.id === id       
+    })
+    res.status(200).send(result)
+})
+
+app.get("/users/:id/purchases", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const result = purchases.filter((purchase)=>{
+        return purchase.userId.toLowerCase().includes(id.toLowerCase())
+    })
+    res.status(200).send(result)
+})
+
+app.delete("/user/:id", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const userIndex = users.findIndex((user)=>{
+        return user.id === id
+    })
+
+    if(userIndex >=0){
+        users.splice(userIndex, 1)
+        res.status(200).send("Usuário excluído com sucesso")
+    }else{
+        res.status(404).send("Usuário não encontrado")
+    }
+})
+
+app.delete("/product/:id", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const productIndex = products.findIndex((product)=>{
+        return product.id === id
+    })
+
+    if(productIndex >=0){
+        products.splice(productIndex, 1)
+        res.status(200).send("Produto excluído com sucesso")
+    }else{
+        res.status(404).send("Produto não encontrado")
+    }
+})
+
+app.put("/user/:id", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const newId = req.body.id as string | undefined
+    const newEmail = req.body.email as string | undefined
+    const newPassword = req.body.password as string | undefined
+
+    const userToEdit = users.find((user)=>{
+        return user.id === id
+    })
+
+    if(userToEdit){
+        userToEdit.id = newId || userToEdit.id
+        userToEdit.email = newEmail || userToEdit.email
+        userToEdit.password = newPassword || userToEdit.password
+        res.status(200).send("Cadastro atualizado com sucesso")
+    }else{
+        res.status(404).send("Usuário não encontrado")
+    }
+})
+
+app.put("/product/:id", (req:Request, res: Response)=>{
+    const id = req.params.id
+
+    const newId = req.body.id as string | undefined
+    const newName = req.body.name as string | undefined
+    const newPrice = req.body.price as number | undefined
+    const newCategory = req.body.category as CATEGORIES | undefined
+
+    const product = products.find((product)=>{
+        return product.id === id
+    })
+
+    if(product){
+        product.id = newId || product.id
+        product.name = newName || product.name
+        product.price = isNaN(newPrice) ? product.price : newPrice
+        product.category = newCategory || product.category
+        res.status(200).send("Cadastro atualizado com sucesso")
+    }else{
+        res.status(404).send("Usuário não encontrado")
+    }
+})
