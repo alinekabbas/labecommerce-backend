1e70f45676d2041bf193241d7fc9b02ef7e947ea
alinekabbas <alinekabbas@gmail.com>
2023-01-17 21:11:16 -0300

    :sparkles: feat: planejamento de API usando banco de dados

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -53,3 +53,61 @@ VALUES (
     );
 
 SELECT * FROM products;
+
+-- Get All Users
+SELECT *FROM users;
+
+-- Get All Products
+SELECT * FROM products;
+
+-- Search Product by name
+SELECT * FROM products WHERE name = "ventilador";
+
+-- Create User
+INSERT INTO
+    users(id, email, password)
+VALUES (
+        '04',
+        'usuario@email.com',
+        'U852147'
+    );
+
+-- Create Product
+INSERT INTO
+    products(id, name, price, category)
+VALUES (
+        'p06',
+        'ar-condicionado',
+        1459.99,
+        'ar & ventilação'
+    );
+
+-- Get Products by id
+SELECT * FROM products WHERE id = 'p01';
+
+-- Delete User by i
+DELETE FROM users WHERE id = '04';
+
+-- Delete Product by id
+DELETE FROM products WHERE id = 'p06';
+
+-- Edit User by id
+UPDATE users SET email = 'fulaninho@email.com' WHERE id = '01';
+
+-- Edit Product by id
+UPDATE products SET price = 129.99 WHERE id = 'p01';
+
+-- Get All Users REFATORADO (retorna o resultado ordenado pela coluna email em ordem crescente)
+SELECT *FROM users ORDER BY email ASC;
+
+-- Get All Products versão 1 REFATORADO
+-- (retorna o resultado ordenado pela coluna price em ordem crescente)
+-- (limite o resultado em 20 iniciando pelo primeiro item)
+SELECT * FROM products ORDER BY price ASC LIMIT 20 OFFSET 0;
+
+-- Get All Products versão 2 REFATORADO
+-- (mocke um intervalo de preços, por exemplo entre 100.00 e 300.00)
+-- (retorna os produtos com preços dentro do intervalo mockado em ordem crescente)
+SELECT * FROM products
+WHERE price >= 100 AND price <= 500
+ORDER BY price ASC;
